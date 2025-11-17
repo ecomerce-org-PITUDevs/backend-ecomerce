7d31aaacee4e2539999fa9aac04922fdb99f20f8
l-dstructordqks <s404990099@gmail.com>
2025-11-17 15:30:57 -0500

    feat: implement MongoDB connection using Mongoose (#6) (#13)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { connectDB } from "./utils/db"
+import mongoose from "mongoose";
 
 // dotenv config (.env)
 dotenv.config();
+
+// connection to DB
+connectDB();
 
 // Declaring the server
 const server: Express = express();
@@ -21,6 +26,16 @@ server.use(express.json({limit: '50mb'}));
 server.get('/api/health', (req:Request, res:Response) => {
     res.json({status: "ok"});
 })
+// Test db connection
+server.get("/api/db-check", (req: Request, res: Response) => {
+    const state = mongoose.connection.readyState;
+
+    res.json({
+        connected: state === 1,
+        state,
+
+    });
+});
 
 server.listen(port, () => {
     console.log(`EXPRESS SERVER: Running on http://localhost:${port}`)
