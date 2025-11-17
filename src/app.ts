46d9e6808b15bdd7de56e66f77eb1b381dfb4a0e
l-dstructordqks <s404990099@gmail.com>
2025-11-17 08:47:49 -0500

    feat: set up Express server with basic middlewares (#3) (#12)
    * feat: set up Express server with basic middlewares (#3)
    * feat: adding json middleware

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log('Backend initialized correctly!!!')
+import express, { Express, Request, Response } from "express";
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// dotenv config (.env)
+dotenv.config();
+
+// Declaring the server
+const server: Express = express();
+
+// PORT
+const port: string | number = process.env.PORT || 4000;
+
+// cors config
+server.use(cors());
+
+// json middleware config
+server.use(express.json({limit: '50mb'}));
+
+// Test 'api/health' route
+server.get('/api/health', (req:Request, res:Response) => {
+    res.json({status: "ok"});
+})
+
+server.listen(port, () => {
+    console.log(`EXPRESS SERVER: Running on http://localhost:${port}`)
+})
